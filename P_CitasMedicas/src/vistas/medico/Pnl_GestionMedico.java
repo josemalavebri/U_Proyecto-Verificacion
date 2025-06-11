b137deee2a0b52fcd033507e372647d834a41617
ING-DAVID-777 <falquezpico_steven@outlook.com>
2025-06-11 11:24:53 -0500

    borrado botones

--- P_CitasMedicas/src/vistas/medico/Pnl_GestionMedico.java
+++ P_CitasMedicas/src/vistas/medico/Pnl_GestionMedico.java
@@ -20,21 +20,13 @@ public class Pnl_GestionMedico extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btn_Buscar = new javax.swing.JButton();
-        btn_Resetear = new javax.swing.JButton();
         btn_Nuevo = new javax.swing.JToggleButton();
         jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        txt_Cedula = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         tb_Medico = new javax.swing.JTable();
 
         setPreferredSize(new java.awt.Dimension(808, 425));
         setRequestFocusEnabled(false);
-
-        btn_Buscar.setText("Buscar");
-
-        btn_Resetear.setText("Resetear");
 
         btn_Nuevo.setText("Nuevo");
         btn_Nuevo.addActionListener(new java.awt.event.ActionListener() {
@@ -44,8 +36,6 @@ public class Pnl_GestionMedico extends javax.swing.JPanel {
         });
 
         jLabel2.setText("Medicos Agregados");
-
-        jLabel1.setText("Cedula");
 
         tb_Medico.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -65,40 +55,27 @@ public class Pnl_GestionMedico extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btn_Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(39, 39, 39)
-                        .addComponent(jLabel1)
-                        .addGap(18, 18, 18)
-                        .addComponent(txt_Cedula, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(30, 30, 30)
-                        .addComponent(btn_Buscar)
-                        .addGap(18, 18, 18)
-                        .addComponent(btn_Resetear)
-                        .addContainerGap(168, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 758, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 758, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(279, 279, 279)
+                        .addComponent(btn_Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 21, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btn_Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1)
-                    .addComponent(txt_Cedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btn_Buscar)
-                    .addComponent(btn_Resetear))
-                .addGap(34, 34, 34)
+                .addGap(20, 20, 20)
+                .addComponent(btn_Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
                 .addComponent(jLabel2)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -115,13 +92,9 @@ public class Pnl_GestionMedico extends javax.swing.JPanel {
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_Buscar;
     private javax.swing.JToggleButton btn_Nuevo;
-    private javax.swing.JButton btn_Resetear;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tb_Medico;
-    private javax.swing.JTextField txt_Cedula;
     // End of variables declaration//GEN-END:variables
 }
